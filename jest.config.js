c35106ae4758507a45de2367bc3a8d7916d9cea6
Maxwell Brown <maxwellbrown1990@gmail.com>
2021-09-04 08:16:08 -0400

    chore(printer): upgrade dependencies

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
   coverageDirectory: "coverage",
   collectCoverageFrom: ["packages/**/src/**/*.ts"],
   setupFiles: ["./scripts/jest-setup.ts"],
+  setupFilesAfterEnv: ["@effect-ts/jest/Extend"],
   modulePathIgnorePatterns: [
     "<rootDir>/packages/.*/build",
     "<rootDir>/packages/.*/compiler-debug",
